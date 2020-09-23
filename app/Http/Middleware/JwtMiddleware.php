273b67b2f819724d4896d45cd4c6f370b7ae8700
Ruslan Khasanshin <r.m.khasanshin@gmail.com>
2020-09-23 12:09:50 +0600

    Added refreshing of token

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Exceptions\TokenExpiredException;
@@ -19,22 +20,31 @@ class JwtMiddleware extends BaseMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
-        $respond = function (string $error) {
+        $respondUnauthorized = function (string $error) {
             return response()->common(Response::HTTP_UNAUTHORIZED, null, [$error]);
         };
+        $response = $next($request);
         try {
             if (!JWTAuth::parseToken()->authenticate()) {
-                return $respond("Unable to authenticate user by token");
+                return $respondUnauthorized("Unable to authenticate user by token");
             }
         } catch (TokenExpiredException $e) {
-            return $respond("Token has expired");
+            try {
+                $refreshedToken = JWTAuth::refresh(JWTAuth::getToken());
+                JWTAuth::setToken($refreshedToken)->toUser();
+                $response->header('token', $refreshedToken);
+                $response->header('token_type', 'bearer');
+                $response->header('expires_in', JWTAuth::factory()->getTTL() * 60);
+            } catch (JWTException $e) {
+                return $respondUnauthorized("Token has expired and can not be refreshed");
+            }
         } catch (TokenInvalidException $e) {
-            return $respond("Token is invalid");
+            return $respondUnauthorized("Token is invalid");
         } catch (JWTException $e) {
-            return $respond("Unable to authenticate user by token");
+            return $respondUnauthorized("Unable to authenticate user by token");
         }
-        return $next($request);
+        return $response;
     }
 }
