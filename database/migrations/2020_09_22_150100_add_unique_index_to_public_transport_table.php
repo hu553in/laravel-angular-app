9cc73d266ebec7e1a32786533b8548d0ae15071a
Ruslan Khasanshin <r.m.khasanshin@gmail.com>
2020-09-23 11:16:59 +0600

    Renamed migration

--- database/migrations/2020_09_22_150100_add_unique_index_to_public_transport_table.php
+++ database/migrations/2020_09_22_150100_add_unique_index_to_public_transport_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddPrimaryKeyAndUniqueIndexToPublicTransportTable extends Migration
+class AddUniqueIndexToPublicTransportTable extends Migration
 {
     /**
      * Run the migrations.
